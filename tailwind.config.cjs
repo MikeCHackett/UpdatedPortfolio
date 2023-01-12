2bceeec0e8eee7b69d5a53c75e3287afc37bffd3
Michael Hackett <87140276+MikeCHackett@users.noreply.github.com>
2023-01-12 18:23:31 -0500

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      violet: '#7c3aed',
+      teal: '#2dd4bf',
+      black: '#18181b',
+      cyan: '#a5f3fc',
+      purple: '#f0abfc',
+      gray: '#d1d5db',
+      white: '#fff',
+      slate: '#64748b',
+      blue: '#0284c7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
